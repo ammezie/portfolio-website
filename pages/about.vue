f3cb512bcbb0c56b357f22aaf076544c22fde167
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-09-11 21:56:21 +0100

    add about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-      <h1 class="uk-heading-primary">About Me</h1>
-  </div>
+    <div class="uk-section">
+        <div class="uk-container">
+            <div class="about uk-background-norepeat uk-background-image@s">
+                <h1 class="uk-heading-bullet">About Me</h1>
+                <div class="uk-width-1-2@s">
+                    <p>
+                        My name is Chimezie Enyinnaya (AKA <span class="emphasis">mezie</span>), I’m a software developer based in Lagos, Nigeria. I build modern applications for the web! Though I do some frontend development, I enjoy working as a backend developer. I’m proficient with PHP (CodeIgniter, Laravel), JavaScript, NodeJS (Express, AdonisJS), VueJS. Recently, I have been exploring GraphQL.
+                    </p>
+
+                    <p>
+                        I'm a technical writer, I write ✍🏾 &nbsp;technical articles and tutorials for various platforms including <a href="https://scotch.io/@mezie" target="_blank">Scotch.io</a> and <a href="https://blog.pusher.com/author/mezie" target="_blank">Pusher</a>. I share my knowledge and experience about software development and everything in between on my <a href="http://blog.chimezieenyinnaya.xyz" target="_blank">blog</a> and I also contribute to <a href="https://github.com/ammezie" target="_blank">open source</a>.
+                    </p>
+
+                    <p>
+                        When I'm not programming, you'll find me listening to music or watching movies, yeah! you read right, I'm a movie 🎬 &nbsp;lover. I love playing football ⚽ ,&nbsp;volleyball 🏐 &nbsp;and table tennis 🏓 &nbsp;too (been a while I do these though). 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,5 +29,13 @@
 
 
 <style scoped>
+    .emphasis {
+        color: #eb3777
+    }
 
+    .about {
+        background-image: url('~/assets/images/mezie.png');
+        background-size: auto 450px;
+        background-position: 80% 0;
+    }
 </style>
