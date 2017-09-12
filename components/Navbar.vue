0d9a4dd971c709f93d8dbde38a49653113edbe9f
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-09-12 13:49:27 +0100

    add blog placeholder

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,7 +11,7 @@
                 <li class="uk-active"><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/work">Work</a></li>
-                <li><a href="http://blog.chimezieenyinnaya.xyz">Blog</a></li>
+                <li><a href="/blog">Blog</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </div>
---
+++ pages/blog.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="uk-section uk-section-xlarge uk-flex uk-flex-middle uk-text-center">
+        <div class="uk-width-1-1">
+            <div class="uk-container">
+                <p class="uk-text-lead">
+                    Coming soon...
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
